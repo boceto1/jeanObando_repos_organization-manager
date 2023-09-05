6cdd7f40459d05a437eb4eb571a52c6cbb878693
boceto1 <janka.obando@outlook.es>
2023-09-04 23:31:18 -0500

    feat: draft Organization Module
    Create the Organization Module using the NestCLI.
    It includes the whole CRUD structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MockApiModule } from './mock-api/mock-api.module';
 import { MockApiController } from './mock-api/mock-api.controller';
 import { MockApiService } from './mock-api/mock-api.service';
+import { OrganizationModule } from './organization/organization.module';
 
 @Module({
-  imports: [MockApiModule],
+  imports: [MockApiModule, OrganizationModule],
   controllers: [MockApiController],
   providers: [MockApiService],
 })
