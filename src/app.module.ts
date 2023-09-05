38c9ec3fbc83f123404d7492b9e79af6a7881012
boceto1 <janka.obando@outlook.es>
2023-09-04 23:44:40 -0500

    chore: Configure DB Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MockApiModule } from './mock-api/mock-api.module';
 import { MockApiController } from './mock-api/mock-api.controller';
 import { MockApiService } from './mock-api/mock-api.service';
 import { OrganizationModule } from './organization/organization.module';
 
+const ORM_MODULE = TypeOrmModule.forRoot({
+  type: 'mysql',
+  host: '127.0.0.1',
+  port: 3306,
+  username: 'root',
+  password: '',
+  database: 'organization-manager',
+  entities: [],
+  synchronize: true,
+});
+
 @Module({
-  imports: [MockApiModule, OrganizationModule],
+  imports: [ORM_MODULE, MockApiModule, OrganizationModule],
   controllers: [MockApiController],
   providers: [MockApiService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
