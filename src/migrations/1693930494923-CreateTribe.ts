460b733079dd54a96ffcf93a3b6e9272078f08c7
boceto1 <janka.obando@outlook.es>
2023-09-05 11:29:15 -0500

    feat: create Tribe Migration

---
+++ src/migrations/1693930494923-CreateTribe.ts
@@ -0,0 +1,62 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateTribe1693930494923 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'tribe',
+        columns: [
+          {
+            name: 'idTribe',
+            type: 'int',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '50',
+          },
+          {
+            name: 'status',
+            type: 'int',
+          },
+        ],
+      }),
+      true,
+    );
+
+    await queryRunner.addColumn(
+      'tribe',
+      new TableColumn({
+        name: 'idOrganization',
+        type: 'int',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'tribe',
+      new TableForeignKey({
+        columnNames: ['idOrganization'],
+        referencedColumnNames: ['idOrganization'],
+        referencedTableName: 'organization',
+        onDelete: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const table = await queryRunner.getTable('tribe');
+    const foreignKey = table.foreignKeys.find(
+      (fk) => fk.columnNames.indexOf('idOrganization') !== -1,
+    );
+    await queryRunner.dropForeignKey('idOrganization', foreignKey);
+    await queryRunner.dropColumn('tribe', 'idOrganization');
+    await queryRunner.dropTable('tribe');
+  }
+}
