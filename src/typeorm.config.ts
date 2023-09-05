d153c89354360d15f9c43c206228a1f209987090
boceto1 <janka.obando@outlook.es>
2023-09-05 11:08:13 -0500

    chore: Configure DB migrations
    Configure Migrations with TypeOrm and add scripts

---
+++ src/typeorm.config.ts
@@ -0,0 +1,27 @@
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
+import { DataSource } from 'typeorm';
+import { SeederOptions } from 'typeorm-extension';
+import { DataSourceOptions } from 'typeorm/data-source';
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+
+ConfigModule.forRoot({
+  envFilePath: '.env',
+});
+
+export const DB_OPTIONS = {
+  type: 'mysql' as MysqlConnectionOptions['type'],
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: parseInt(String(process.env.DATABASE_PORT), 10) || 3306,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_DATABASE,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [join(__dirname, 'migrations/**/*{.ts,.js}')],
+  migrationsTableName: 'migrations',
+  seeds: ['./seeds/**/*{.ts,.js}'],
+};
+
+export const source = new DataSource(
+  DB_OPTIONS as DataSourceOptions & SeederOptions,
+);
